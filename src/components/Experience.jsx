f144ce9eb85b00f497b57acb5f48548ce1c892e8
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-02 00:15:02 +0530

    Added working skeletal structure showing successful CRUD

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,54 @@
 import "./Experience.css"
+import { useState } from "react";
 
 function Experience(){
+    const [text, setText]=useState({
+        company: '',
+        startDate: '',
+        endDate: ''
+    });
+    const [display, setDisplay]=useState({});
+    const [editing, setEditing]=useState(true);
 
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+
+        setText(prevText=>({
+            ...prevText, [name]:value
+        }));
+    }
+
+    const handleSubmit=()=>{
+        setDisplay(text);
+        setEditing(false);
+    }
+
+    const handleEdit=()=>{
+        setText(display);
+        setEditing(true);
+    }
+
+    return(
+        <div>
+            {
+                editing ? (
+                    <div>
+                        <input type="text" name="company" value={text.company} onChange={handleChange}/>
+                        <input type="text" name="startDate" value={text.startDate} onChange={handleChange}/>
+                        <input type="text" name="endDate" value={text.endDate} onChange={handleChange}/>
+                        <button onClick={handleSubmit}>Submit</button>
+                    </div>
+                ):(
+                    <div>
+                        <div>{display.company}</div>
+                        <div>{display.startDate}</div>
+                        <div>{display.endDate}</div>
+                        <button onClick={handleEdit}>Edit</button>
+                    </div>
+                )
+            }
+        </div>
+    );
 }
 
 export default Experience;
