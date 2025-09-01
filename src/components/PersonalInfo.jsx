3c67b0f8aebe15c40fcf2bda18a4693df2b08d7c
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-01 22:55:02 +0530

    Added working skeletal structure showing successful CRUD

--- src/components/PersonalInfo.jsx
+++ src/components/PersonalInfo.jsx
@@ -2,9 +2,22 @@ import "./PersonalInfo.css"
 import { useState } from "react";
 
 function PersonalInfo(){
-    const [text, setText] = useState("");
-    const [display, setDisplay] = useState("");
+    const [text, setText] = useState({
+        name: "",
+        email: "",
+        phone: ""
+    });
+    const [display, setDisplay] = useState({});
     const [editing, setEditing] = useState(true);
+
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+
+        setText(prevState=>({
+            ...prevState, [name]:value
+        }));
+    }
+
     const handleSubmit=()=>{
         setDisplay(text);
         setEditing(false);
@@ -20,13 +33,17 @@ function PersonalInfo(){
             {
                 editing ? (
                     <div>
-                        <input type="text" value={text} onChange={(e)=> setText(e.target.value)}/>
+                        <input type="text" value={text.name} name="name" onChange={handleChange}/>
+                        <input type="text" value={text.email} name="email" onChange={handleChange}/>
+                        <input type="text" value={text.phone} name="phone" onChange={handleChange}/>
                         <button onClick={handleSubmit}>Submit</button>
                     </div>
                 ):
                 (
                     <div>
-                        <div>{display}</div>
+                        <div>{display.name}</div>
+                        <div>{display.email}</div>
+                        <div>{display.phone}</div>
                         <button onClick={handleEdit}>Edit</button>
                     </div>
                 )
