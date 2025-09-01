7cbfc8857d6d37affd244c7e7a8a0df6e572c367
avanish1207 <159261997+avanish1207@users.noreply.github.com>
2025-09-01 23:44:27 +0530

    Added working skeletal structure showing successful CRUD

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,7 +1,57 @@
 import "./Education.css";
+import { useState } from "react";
 
 function Education(){
+    const [text, setText]= useState({
+        college:"",
+        year:"",
+        cgpa:""
+    });
+    const [display, setDisplay]=useState({});
+    const [editing, setEditing]=useState(true);
 
+    const handleChange=(e)=>{
+        const {name, value}=e.target;
+
+        setText(prevText=> ({
+            ...prevText, [name]: value
+        }))
+    }
+
+    const handleSubmit=()=>{
+        setDisplay(text);
+        setEditing(false);
+    }
+
+    const handleEdit=()=>{
+        setText(display);
+        setEditing(true);
+    }
+
+    return(
+        
+        <div>
+            {
+                editing?(
+                    <div>
+                        <input type="text" name="college" value={text.college} onChange={handleChange}/>
+                        <input type="text" name="year" value={text.year} onChange={handleChange}/>
+                        <input type="text" name="cgpa" value={text.cgpa} onChange={handleChange}/>
+                        <button onClick={handleSubmit}>Submit</button>
+                    </div>
+                ):(
+                    <div>
+                        <div>{display.college}</div>
+                        <div>{display.year}</div>
+                        <div>{display.cgpa}</div>
+                        <button onClick={handleEdit}>Edit</button>
+                    </div>
+                )
+            }
+
+        </div>
+        
+    );
 }
 
 export default Education;
